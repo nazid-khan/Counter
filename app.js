c6981d6afec199d4c7cb979177b00a2b4bd16937
nazid <nazidk773@gmail.com>
2024-07-03 12:36:51 +0530

    update javaScript

--- app.js
+++ app.js
@@ -2,27 +2,33 @@
 let count = 0;
 
 // select value and buttons
-const value = document.getElementById("#value");
+const value = document.getElementById("value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function(btn){
+    
     btn.addEventListener("click",function(event){
         const styles = event.currentTarget.classList;
+        
         if(styles.contains("decrease")){
+            // value Decrease by 1
             count--;
         }
         else if(styles.contains("increase")){
+            // value Increase by 1
             count++;
         }
         else{
+            // value Reset 
             count = 0;
         }
         value.textContent = count;
-        console.log(count);
-        if(count > 0){
+
+        // color change on negatives & positives
+        if(count < 0){
             value.style.color = "red";
         }
-        if(count < 0){
+        if(count > 0){
             value.style.color = "green";
         }
         if(count === 0){
