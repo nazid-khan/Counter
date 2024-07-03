b220858a9d44ba6c713648b17088f279b664d2ab
nazid <nazidk773@gmail.com>
2024-07-03 11:26:20 +0530

    count number

---
+++ app.js
@@ -0,0 +1,23 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.getElementById('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(event){
+        const styles = event.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        //value.textContent = count;
+        console.log(count);
+    });
+});
